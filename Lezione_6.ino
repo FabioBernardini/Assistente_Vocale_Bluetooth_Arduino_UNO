f8bc14a4ae538952e11c45f42952573e32838e6e
Fabio Bernardini <56018807+FabioBernardini@users.noreply.github.com>
2021-04-17 15:22:53 +0200

    Add files via upload

---
+++ Lezione_6.ino
@@ -0,0 +1,44 @@
+//Accendiamo e spegnaimo led a distanza tramite applicazione su tablet
+
+#include <SoftwareSerial.h>
+
+//Variabile per la scheda Bluetooth
+SoftwareSerial bt(10, 11); // RX | TX
+char val=-1;//carattere inviato da applicazione android per modificare stato LED
+
+void setup()
+{
+  pinMode(3,OUTPUT);
+  //Impostiamo il baude Rate
+  Serial.begin(9600);
+  bt.begin(9600);  
+  Serial.println("Disponibile per la connessione");
+ 
+}
+ 
+void loop()
+{
+ 
+  //Ci manteniamo in ascolto continuo di dati provenienti dal modulo bluetooth
+  if (bt.available()){
+    val=bt.read();
+    if (val=='1'){ //Accendo il LED
+      digitalWrite(3,HIGH);
+      //Invio tramite BT la stringa che comunica l'accensione del LED
+      bt.println("Stato LED: Acceso");
+    }
+    else if (val=='0'){
+      digitalWrite(3,LOW); //Spengo il LED
+      //Invio tramite BT la stringa che comunica lo spegnimento del LED
+      bt.println("Stato LED: Spento");
+    }
+    
+    Serial.println("Stato LED: "+val);
+    
+  }
+    
+    
+    
+  
+  
+}
